14aaadf7160ed429c4cb1e28d95960d424ea8c80
NOCTURNO1976 <dr.whaler@gmail.com>
2025-11-01 22:36:27 -0400

    Create BootReceiver.kt

---
+++ app/src/main/java/com/prodestino/manaus/bg/BootReceiver.kt
@@ -0,0 +1,18 @@
+package com.prodestino.manaus.bg
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        // Sobe o serviço em silêncio após BOOT/UPDATE
+        val it = Intent(context, LocationForegroundService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(it)
+        } else {
+            context.startService(it)
+        }
+    }
+}
