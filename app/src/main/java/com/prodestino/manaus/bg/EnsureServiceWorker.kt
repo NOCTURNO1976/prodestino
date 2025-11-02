8943c1775d1601b086442b0f50ffec00d55be09c
NOCTURNO1976 <dr.whaler@gmail.com>
2025-11-01 22:39:06 -0400

    Create EnsureServiceWorker.kt

---
+++ app/src/main/java/com/prodestino/manaus/bg/EnsureServiceWorker.kt
@@ -0,0 +1,20 @@
+package com.prodestino.manaus.bg
+
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+
+class EnsureServiceWorker(appContext: Context, params: WorkerParameters)
+    : CoroutineWorker(appContext, params) {
+    override suspend fun doWork(): Result {
+        val it = Intent(applicationContext, LocationForegroundService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            applicationContext.startForegroundService(it)
+        } else {
+            applicationContext.startService(it)
+        }
+        return Result.success()
+    }
+}
